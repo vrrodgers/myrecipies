0d918840a1e89ee8a6ac0fa243f41245d819833d
vrrodgers <trulymissvicky1982@gmail.com>
2017-04-10 13:01:13 -0500

    complete authentication

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,5 +1,6 @@
 class ChefsController < ApplicationController
   before_action :set_chef, only: [:show, :edit, :update, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def index
     @chefs = Chef.paginate(page: params[:page], per_page: 4)
@@ -12,6 +13,7 @@ class ChefsController < ApplicationController
   def create
     @chef = Chef.new(chef_params)
     if @chef.save
+      session[:chef_id] = @chef.id
       flash[:success] = "Welcome #{@chef.chefname} to Myrecipes App!"
       redirect_to chef_path(@chef)
     else
@@ -20,7 +22,6 @@ class ChefsController < ApplicationController
   end
 
   def show
-    @chef = Chef.find(params[:id])
     @chef_recipes= @chef.recipes.paginate(page: params[:page], per_page: 4)
   end
 
@@ -37,7 +38,6 @@ class ChefsController < ApplicationController
     end
   end
   def destroy
-    @chef= Chef.find(params[:id])
     @chef.destroy
     flash[:danger] = "Chef and all associated recipes have been deleted!"
     redirect_to chefs_path
@@ -54,4 +54,14 @@ class ChefsController < ApplicationController
   def chef_params
     params.require(:chef).permit(:chefname, :email, :password, :password_confirmation)
   end  
+
+  def require_same_user
+    if current_chef != @chef
+      flash[:danger] = "You can only edit ir delete your own account"
+      redirect_to chefs_path
+    end
+  end
+    
+
+
 end
