5720c983e7136fc08dd6e60ccc1a62a4d5f74d4e
vrrodgers <trulymissvicky1982@gmail.com>
2017-04-12 13:28:16 -0500

    add comment model and association

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,5 +1,6 @@
 class Chef < ApplicationRecord
 has_many :recipes, dependent: :destroy
+has_many :comments, dependent: :destroy
 has_secure_password
 validates :password, presence: true, length: {minimum: 5},allow_nil: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170412175041) do
+ActiveRecord::Schema.define(version: 20170412181433) do
 
   create_table "chefs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "chefname"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20170412175041) do
     t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.boolean  "admin",           default: false
+  end
+
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "description", limit: 65535
+    t.integer  "chef_id"
+    t.integer  "recipe_id"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
   create_table "ingredients", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
